256928eabe0d8babbff168398f5b6aa67d688ef8
LeeeeSuHyeon <tngus0673@naver.com>
2023-11-04 12:17:41 +0900

    first commit

---
+++ router/personRouter.js
@@ -0,0 +1,31 @@
+// 201935312 이수현 - personRouter.js
+
+const express = require('express');
+var router = express.Router()
+
+var person = require('../lib/person');
+
+router.get('/view/:vu',(req, res)=>{
+    person.view(req, res);
+}); 
+
+router.get('/create',(req,res)=>{
+    person.create(req,res);
+})
+
+router.post('/create_process',(req,res)=>{
+    person.create_process(req,res);
+})
+
+router.get('/update/:id',(req,res)=>{
+    person.update(req,res);
+})
+router.post('/update_process',(req,res)=>{
+    person.update_process(req,res);
+})
+router.get('/delete/:id',(req,res)=>{
+    person.delete_process(req,res);
+})
+
+module.exports = router;
+
---
+++ router/rootRouter.js
@@ -0,0 +1,15 @@
+// 201935312 이수현 router/rootRouter.js
+
+const express = require('express');
+var router = express.Router()
+
+var shop = require('../lib/shop');
+
+
+router.get('/',(req, res)=>{
+
+    shop.home(req, res);
+}); 
+
+module.exports = router;
+
