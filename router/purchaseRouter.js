e43aaa74948501ee9bf680f9ef0882b8c005905a
LeeeeSuHyeon <tngus0673@naver.com>
2023-11-16 19:33:00 +0900

    구매 로직 구현

--- router/purchaseRouter.js
+++ router/purchaseRouter.js
@@ -9,6 +9,10 @@ router.get('/',(req, res)=>{
     purchase.home(req, res);
 }); 
 
+router.get('/cart',(req, res)=>{
+    purchase.cart(req, res);
+}); 
+
 router.get('/detail/:merId',(req, res)=>{
     purchase.detail(req, res);
 }); 
@@ -16,6 +20,15 @@ router.get('/detail/:merId',(req, res)=>{
 router.post('/purchase_process',(req, res)=>{
     purchase.purchase_process(req, res);
 }); 
+
+router.get('/cart_process/:mer_id',(req, res)=>{
+    purchase.cart_process(req, res);
+}); 
+
+router.get('/cancel/:pur_id',(req, res)=>{
+    purchase.cancel_process(req, res);
+}); 
+
 
 
 module.exports = router;
